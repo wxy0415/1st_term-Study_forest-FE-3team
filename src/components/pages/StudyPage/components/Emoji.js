34e00ed16401fa41e9cad2a7d64791a80537cc4c
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-08-22 08:41:10 +0900

    feat : 이모지 선택창 추가 & 관련 라이브러리 추가 설치
    이모지를 선택하는 로직까지 추가
    그 과정에서 관련 라이브러리 추가 설치

--- src/components/pages/StudyPage/components/Emoji.js
+++ src/components/pages/StudyPage/components/Emoji.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+
 import "./Emoji.css";
 
 export function Emoji() {
-  return <div className="emoji">emoji</div>;
+  const [isPickerOpen, setIsPickerOpen] = useState(false);
+  const [tempEmoji, setTempEmoji] = useState("이모지 출력 영역");
+
+  const handleAddEmoji = () => {
+    setIsPickerOpen(!isPickerOpen);
+  };
+
+  const handleSelectedEmoji = (e) => {
+    setIsPickerOpen(false);
+    setTempEmoji(e.native);
+    alert("기능 추가 예정 / log 확인");
+    console.log(e);
+  };
+
+  const showTempEmoji = () => {
+    return tempEmoji;
+  };
+
+  return (
+    <emoji className="flex-row">
+      <div className="font20">{showTempEmoji()}</div>
+      <div>
+        <svg className="emoji__btn-add" onClick={handleAddEmoji} />
+        {isPickerOpen && (
+          <Picker data={data} onEmojiSelect={handleSelectedEmoji} />
+        )}
+      </div>
+    </emoji>
+  );
 }
 
 export default Emoji;
